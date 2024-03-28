1724097ec573e134dc0e799645907de36a5eafaf
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2024-03-28 15:28:23 -0700

    Time: 823 ms (91.95%) | Memory: 21.6 MB (70.69%) - LeetSync

---
+++ 2029-minimize-product-sum-of-two-arrays/minimize-product-sum-of-two-arrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minProductSum(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1.sort()
+
+        nums2.sort(reverse=True)
+
+        sum = 0
+
+        for i in range(len(nums1)):
+            sum += nums1[i] * nums2[i]
+        
+        return sum
